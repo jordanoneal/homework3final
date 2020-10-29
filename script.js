776fee6b5c6798ae23fc562278094b1a019814a8
jordan o'neal <joneal6694@gmail.com>
2020-10-28 21:12:38 -0600

    refactor code

--- script.js
+++ script.js
@@ -1,64 +1,130 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+// variables for password requirements
+let lowercaseLetters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let uppercaseLetters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+let specialCharacters = [
+  "!",
+  "?",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "<",
+  ">",
+  "~",
+  "`",
+];
+let numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-//variables for password characters
-var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specialCharacters = ['!', '?', '@', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', '~', '`'];
-var numberCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// array for all characters
+let allCharacters = [];
 
-//array for all characters
-var allCharacters = [];
-
-//assign length to prompt
-var length = Number(prompt("How many characters would you like your password to be? Choose between 8 and 128 characters"));
+// assign length to prompt
+let length = prompt(
+  "How many characters would you like your password to be? Choose between 8 and 128 characters"
+);
 
 // will repeat prompt if length does not meet requirements.
 while (isNaN(length) || length < 8 || length > 128)
-    length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+  length = prompt(
+    "Length must be 8-128 characters. How many characters would you like your password to be?"
+  );
 
-//alert user they must select at least one character
 while (allCharacters.length === 0) {
-    var uppercase = confirm("Would you like to use uppercase letters?");
-    if (uppercase) {
-        allCharacters.push(uppercaseLetters);
-    }
-    var lowercase = confirm("Would you like to use lowercase letters?");
-    if (lowercase) {
-        allCharacters.push(lowercaseLetters);
-    }
-    var numbers = confirm("Would you like to use numbers?");
-    if (numbers) {
-        allCharacters.push(numberCharacters);
-    }
-    var special = confirm("Would you like to use special characters?");
-    if (special) {
-        allCharacters.push(specialCharacters);
-    }
-    if (allCharacters.length === 0) {
-        alert("choose at least one!");
-    }
-
+  let uppercase = confirm("Would you like to use uppercase letters?");
+  if (uppercase) {
+    allCharacters.push(uppercaseLetters);
+  }
+  let lowercase = confirm("Would you like to use lowercase letters?");
+  if (lowercase) {
+    allCharacters.push(lowercaseLetters);
+  }
+  let numbers = confirm("Would you like to use numbers?");
+  if (numbers) {
+    allCharacters.push(numberCharacters);
+  }
+  let special = confirm("Would you like to use special characters?");
+  if (special) {
+    allCharacters.push(specialCharacters);
+  }
+  if (allCharacters.length === 0) {
+    alert("choose at least one!");
+  }
 }
 
-//function to generatePassword
 function generatePassword() {
-    var password = "";
+  let password = "";
 
-    for (var i = 0; i < length; i++) {
-        var randomNumber = Math.floor(Math.random() * allCharacters.length);
-        var tempArr = allCharacters[randomNumber];
-        var randomNumber2 = Math.floor(Math.random() * tempArr.length);
-        var newCharacter = tempArr[randomNumber2];
+  for (let i = 0; i < length; i++) {
+    let randomNumber = Math.floor(Math.random() * allCharacters.length);
+    let tempArr = allCharacters[randomNumber];
+    let randomNumber2 = Math.floor(Math.random() * tempArr.length);
+    let newCharacter = tempArr[randomNumber2];
 
-        password += newCharacter;
-    }
+    password += newCharacter;
+  }
 
-    console.log(password);
-
-    var textArea = document.getElementById("password");
-    textArea.value = password;
+  let textArea = document.getElementById("password");
+  textArea.value = password;
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
